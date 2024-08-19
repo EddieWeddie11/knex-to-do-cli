9b48862acc9cb3440000f60ca2c0643582bf28ea
edward soung <edward.soung42@gmail.com>
2024-08-19 12:01:23 +1200

    Finished Step 2

---
+++ seeds/test-tasks.js
@@ -0,0 +1,12 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function seed(knex) {
+  // Deletes ALL existing entries
+  await knex('todos').del()
+  await knex('todos').insert([
+    { id: 1, tasks: 'hello' },
+    { id: 2, tasks: 'hi' },
+  ])
+}
